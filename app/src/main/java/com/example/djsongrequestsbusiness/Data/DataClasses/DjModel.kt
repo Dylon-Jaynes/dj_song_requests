3276b9edb29e67692284372cf90b67ac6d25e9b1
Dylon Jaynes <dylonjaynes@gmail.com>
2021-10-08 12:59:07 -0600

    Attempting to get previous files back to the way they were.

--- app/src/main/java/com/example/djsongrequestsbusiness/Data/DataClasses/DjModel.kt
+++ app/src/main/java/com/example/djsongrequestsbusiness/Data/DataClasses/DjModel.kt
@@ -1,6 +1,6 @@
 package com.example.djsongrequestsbusiness.Data.DataClasses
 
-data class DJ(
+data class DjModel(
     val firstName: String = "null",
     var lastName: String = "null",
     var email: String = "null",
