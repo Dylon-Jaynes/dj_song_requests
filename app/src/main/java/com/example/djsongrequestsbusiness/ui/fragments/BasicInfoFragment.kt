8fe82f46f51147f3e35a1b9cead705f07c2c82f5
Dylon Jaynes <dylonjaynes@gmail.com>
2021-11-02 14:48:16 -0600

    Implementing LiveData<Event> in viewModel. View correctly executing events based on updates from viewModel.

--- app/src/main/java/com/example/djsongrequestsbusiness/ui/fragments/BasicInfoFragment.kt
+++ app/src/main/java/com/example/djsongrequestsbusiness/ui/fragments/BasicInfoFragment.kt
@@ -1,34 +1,24 @@
 package com.example.djsongrequestsbusiness.ui.fragments
 
-import android.content.ContentValues
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import com.example.djsongrequestsbusiness.R
 import com.example.djsongrequestsbusiness.data.dataClasses.LoginModel
 import com.example.djsongrequestsbusiness.databinding.FragmentBasicInfoBinding
 import com.example.djsongrequestsbusiness.ui.viewModels.BasicInfoViewModel
-import com.google.firebase.auth.FirebaseAuthEmailException
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import androidx.annotation.NonNull
-import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-
+import androidx.lifecycle.ViewModel
 
 class BasicInfoFragment : Fragment() {
+
+    private lateinit var viewModel: BasicInfoViewModel
 
     private var _binding: FragmentBasicInfoBinding? = null
     // This property is only valid between onCreateView and onDestroyView
@@ -46,28 +36,38 @@ class BasicInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val viewModel: BasicInfoViewModel = ViewModelProvider(this)[BasicInfoViewModel::class.java]
+
+        // Initialize the viewModel
+        viewModel = ViewModelProvider(this)[BasicInfoViewModel::class.java]
+
+        // Display the result of the Task<AuthResult> returned from Firebase.
+        viewModel.displayCreateUserResult.observe(viewLifecycleOwner, Observer { it ->
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+        // Navigates to the next fragment if the event has never been handled before.
+        viewModel.navigateToDjIdFrag.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let {
+                Navigation.findNavController(view).navigate(R.id.next_destination)
+            }
+        })
 
         // Sends user back to login onBackPressed.
         val callback = requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             Navigation.findNavController(view).navigate(R.id.to_login_frag)
         }
 
-//        binding.buttonContinue.isEnabled = false
+        displayCreateUserResult(view)
 
-//        binding.edittextUsername.addTextChangedListener {
-//            val username = binding.edittextUsername.text.toString().trim()
-//            /* TODO add code to enable the continue button if username, email, and
-//                password aren't empty. */
-//        }
+    }
 
+    private fun displayCreateUserResult(view: View) {
         binding.buttonContinue.setOnClickListener(View.OnClickListener {
             val newLogin = LoginModel(binding.edittextEmail.text.toString().trim(), binding.edittextPassword.text.toString().trim())
             viewModel.onClickSignUp(newLogin)
-            val authStatusObserver = Observer<String> { newAuthString ->
-                Toast.makeText(requireContext(), newAuthString, Toast.LENGTH_SHORT).show()
-                Navigation.findNavController(view).navigate(R.id.next_destination)
-            }
         })
     }
 
--- app/src/main/java/com/example/djsongrequestsbusiness/ui/viewModels/BasicInfoViewModel.kt
+++ app/src/main/java/com/example/djsongrequestsbusiness/ui/viewModels/BasicInfoViewModel.kt
@@ -4,10 +4,13 @@ import android.app.Application
 import android.content.ContentValues
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.djsongrequestsbusiness.Event
 
 import com.example.djsongrequestsbusiness.data.dataClasses.LoginModel
 import com.example.djsongrequestsbusiness.data.repositories.UserRepository
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -15,9 +18,16 @@ import com.google.firebase.auth.FirebaseAuth
 
 class BasicInfoViewModel(application: Application): AndroidViewModel(application) {
 
+    private val _navigateToDjIdFrag = MutableLiveData<Event<Boolean>>()
+    val navigateToDjIdFrag: LiveData<Event<Boolean>>
+        get() = _navigateToDjIdFrag
+
+    private val _displayCreateUserResult = MutableLiveData<Event<String>>()
+    val displayCreateUserResult: LiveData<Event<String>>
+        get() = _displayCreateUserResult
 
     private val auth = FirebaseAuth.getInstance()
-    private lateinit var authStatusMessage: MutableLiveData<String>
+
     fun onClickSignUp(loginModel: LoginModel) {
         val userRepository = UserRepository(auth)
         val userSignUpResult = userRepository.userSignUp(loginModel.email, loginModel.password)
@@ -29,11 +39,12 @@ class BasicInfoViewModel(application: Application): AndroidViewModel(application
             if (result.isSuccessful) {
                 // Sign in success, update UI with the signed-in user's information
                 Log.d(ContentValues.TAG, "createUserWithEmail:success")
-                authStatusMessage.value = "Your account has been successfully created!"
+                _displayCreateUserResult.value = Event("Your account has been successfully created!")
+                _navigateToDjIdFrag.value = Event(true)
             } else {
                 // If sign in fails, display a message to the user.
-                Log.w(ContentValues.TAG, "createUserWithEmail:failure", result.exception)
-                authStatusMessage.value = "User account creation failed."
+                Log.w(ContentValues.TAG, "createUserWithEmail:failure")
+                _displayCreateUserResult.value = Event("User account creation failed.")
 //                binding.edittextEmail.error = "This email address is already in use by another account."
             }
         }
